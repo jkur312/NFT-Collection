3a569039c89fd06e126d04fb6a30f4616694d5a4
Jacob Kurian <jkur@umich.edu>
2023-06-13 10:36:17 -0400

    trying to update nft contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xd810ea32C4A10BD223C1A8579A1E21c7D6dB7Db2";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-omega-eosin.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-2-hazel.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
